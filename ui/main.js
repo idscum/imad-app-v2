92e4addfab4cb25d1957385651dfbaa437430dd9
idscum <sreerag.sundaram@gmail.com>
2017-02-15 21:29:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,35 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
-    var names= ['name1','name2','name3','name4'];
-    var list='';
     
-    for( i=0; i<names.length; i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
+     //creating the req
+    var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)//request successful
+          {
+            var names= request.responseText;
+            names= JSON.parse(names);
+            
+            var list='';
     
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+            for( i=0; i<names.length; i++)
+            {
+                list += '<li>' + names[i] + '</li>';
+            }
+    
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+          }
+      }
+    };
+  
+  //making the req
+  request.open('GET', "http://idscum.imad.hasura-app.io/submit-name?name?=" + name, true);
+  request.send(null); 
+  
+  
+    
 };

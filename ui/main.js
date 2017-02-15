b9863201450565dae9b0228b9e4c8ac46dd1a3a2
idscum <sreerag.sundaram@gmail.com>
2017-02-15 20:21:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,23 @@
 var button = document.getElementById('counter');
 
-var counter = 0;
-
 button.onclick = function() {
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-
-    
-}
+    //creating the req
+    var request = new AMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)//request successful
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  
+  //making the req
+  request.open('GET', "http://idscum.imad.hasura-app.io/counter", true);
+  request.send(null); 
+};

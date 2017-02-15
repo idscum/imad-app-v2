9cab68844ce288fdc9cf55c6dda9dfd9b9f26895
idscum <sreerag.sundaram@gmail.com>
2017-02-15 21:53:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,9 +38,9 @@ submit.onclick = function() {
           if(request.status === 200)//request successful
           {
             var names= request.responseText;
-            names= JSON.parse(names);
+            names = JSON.parse(names);
             
-            var list='';
+            var list = '';
     
             for(var i=0; i<names.length; i++)
             {

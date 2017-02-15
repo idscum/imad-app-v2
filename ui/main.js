f7c9c5ec3afac0a7f8f829a746af3d3e59bcc850
idscum <sreerag.sundaram@gmail.com>
2017-02-15 18:45:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,15 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "Kimmy Kardy";
+
+var img = document.getElementById("madi");
+
 var marginRight = 0;
 function moveLeft() {
   marginRight = marginRight + 20;
     img.style.marginRight = marginRight + "px";
 }
-var img = document.getElementById("madi");
+
 img.onclick = function() {
     var interval = setInterval(moveLeft,100);
 };
